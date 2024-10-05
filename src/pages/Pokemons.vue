100b363f637c2a76627543063cfd1b957ac03b65
leonardoMetaway <leonardo@metaway.com.br>
2024-10-05 12:56:50 -0300

    feat: adicionado componente filtro na listagem

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,47 +1,64 @@
+@ -0,0 +1,335 @@
 <template>
-  <div
-    id="list-wrapper"
-    ref="listWrapper"
-  >
-    <div
-      v-if="pokemonsEncontrados.length"
-      id="pokemon-list"
-    >
-      <PokemonCard
-        v-for="pokemon in pokemonsEncontrados"
-        :id="`card-${pokemon.name}`"
-        :key="`card-${pokemon.name}`"
-        :class="{'selected' : pokemon.name === pokemonSelecionado.name}"
-        :pokemon="pokemon"
-        @selecionar-pokemon="selecionarPokemon"
+  <div id="tela-listagem-pokemons">
+    <div id="search-wrapper">
+      <SearchInput
+        v-model="searchText"
+        v-model:selected-type="searchType"
       />
     </div>
-
     <div
-      v-if="isLoading"
-      id="loading-indicator"
+      id="list-wrapper"
+      ref="listWrapper"
     >
-      Carregando...
+      <div
+        v-if="pokemonsFiltrados.length"
+        id="pokemon-list"
+      >
+        <PokemonCard
+          v-for="pokemon in pokemonsFiltrados"
+          :id="`card-${pokemon.name}`"
+          :key="`card-${pokemon.name}`"
+          :class="{'selected' : pokemon.name === pokemonSelecionado.name}"
+          :pokemon="pokemon"
+          @selecionar-pokemon="selecionarPokemon"
+        />
+      </div>
+
+      <div
+        v-if="isLoading"
+        id="loading-indicator"
+      >
+        Carregando...
+      </div>
+
+      <div
+        v-if="!pokemonsFiltrados.length"
+        class="text-center"
+      >
+        Nenhum pokemon foi encontrado...
+      </div>
+
+      <div 
+        v-if="pokemonSelecionado.name" 
+        class="overlay" 
+        @click="selecionarPokemon(pokemonSelecionado)" 
+      />
+
+      <CardDetalhes :pokemon="pokemonSelecionado" />
     </div>
-
-    <div 
-      v-if="pokemonSelecionado.name" 
-      class="overlay" 
-      @click="selecionarPokemon(pokemonSelecionado)" 
-    />
-
-    <CardDetalhes :pokemon="pokemonSelecionado" />
   </div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted, onBeforeUnmount, inject, computed } from 'vue'
+import { ref, onMounted, onBeforeUnmount, inject, computed, watch } from 'vue'
 import { usePokemonStore } from '@/stores/PokemonStore'
 
 import CriarApi from '@/api/index'
 
 import type { PokemonData } from '@/utils/PokemonData'
 
+import SearchInput from '@/components/SearchInput.vue'
 import PokemonCard from '@/components/PokemonCard.vue'
 import CardDetalhes from '@/components/CardDetalhes.vue'
 
@@ -53,6 +70,7 @@ export default {
   name: 'App',
 
   components: {
+    SearchInput,
     PokemonCard,
     CardDetalhes
   },
@@ -67,10 +85,15 @@ export default {
     const api = CriarApi()
     
     const pokemonsEncontrados = ref([])
+    const pokemonsFiltrados = ref([])
+
     const limit = ref(20)
     const offset = ref(0)
     const semMaisPokemons = ref(false)
     const isLoading = ref(false)
+
+    const searchText = ref('')
+    const searchType = ref(null)
 
     const listarPokemons = async () => {
       if (semMaisPokemons.value || isLoading.value) return
@@ -91,6 +114,9 @@ export default {
         } else {
           offset.value += limit.value
         }
+
+        filtrarPokemons()
+
       } catch (err) {
         if (showSnackbar) {
           showSnackbar('Não foi possível carregar novos pokemons', 'red', 2000)
@@ -101,7 +127,43 @@ export default {
       }
     }
 
+    const listarPokemonsPorTipo = async () => {
+      isLoading.value = true
+      try {
+        const resp = await api.get(`/type/${searchType.value}`)
+        const pokemons = resp.data.pokemon.map(p => p.pokemon)
+
+        pokemonsEncontrados.value = pokemons
+        pokemonsFiltrados.value = [...pokemonsEncontrados.value]
+
+        filtrarPokemons()
+      } catch (err) {
+        if (showSnackbar) {
+          showSnackbar('Não foi possível carregar pokemons por tipo', 'red', 2000)
+        }
+        console.error('Não foi possível carregar pokemons por tipo', err)
+      } finally {
+        isLoading.value = false
+      }
+    }
+
+    const filtrarPokemons = () => {
+      if (!searchText.value) {
+        pokemonsFiltrados.value = pokemonsEncontrados.value
+        return
+      }
+
+      const textFilter = searchText.value.toLowerCase()
+
+      pokemonsFiltrados.value = pokemonsEncontrados.value.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(textFilter) || pokemon.id?.toString().includes(textFilter)
+      })
+    }
+
     const handleScroll = () => {
+      // caso esteja filtrando por tipo, o offset não é utilizado
+      if (searchType.value) return
+      
       const scrollTop = window.scrollY
       const windowHeight = window.innerHeight
       const documentHeight = document.documentElement.scrollHeight
@@ -158,6 +220,24 @@ export default {
       cardDetalhes.style.top = `${cy - cardElement.clientHeight / 2}px`
     }
 
+    watch(() => searchText.value, () => {
+      filtrarPokemons()
+    })
+
+    watch(() => searchType.value, (newVal, oldVal) => {
+      if (newVal !== oldVal) {
+        offset.value = 0
+        pokemonsEncontrados.value = []
+      }
+      listWrapper.value.scrollTo({ top: 0, behavior: 'smooth' })
+      if (searchType.value) {
+        listarPokemonsPorTipo()
+      } else {
+        listarPokemons()
+        
+      }
+    })
+
     onMounted(() => {
       listarPokemons()
 
@@ -173,6 +253,9 @@ export default {
     return {
       listWrapper,
       pokemonsEncontrados,
+      pokemonsFiltrados,
+      searchText,
+      searchType,
       pokemonSelecionado,
       semMaisPokemons,
       isLoading,
@@ -183,23 +266,42 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-#list-wrapper
+#tela-listagem-pokemons
   width: 100%
   height: 100%
-  padding-top: 5px
-  margin-top: 10px
-  height: calc(100vh - 60px)
+  margin-top: 16px
+
+#search-wrapper
+  padding-top: 10px
+  padding-bottom: 10px
+  display: flex
+  .v-input
+    margin-left: auto
+    margin-right: auto
+    width: 100%
+    padding-left: 10px
+    padding-right: 10px
+    max-width: 600px
+    
+
+#list-wrapper
+  height: calc(100vh - 129px)
   overflow-y: scroll
 
 #pokemon-list 
   display: grid
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr))
-  margin-left: 250px
-  margin-right: 250px
   gap: 20px
   padding: 20px
 
-  @media (max-width: 1200px)
+  @media (min-width: 1870px)
+    margin-left: 250px
+    margin-right: 250px
+  @media (max-width: 1870px)
+    margin-left: 100px
+    margin-right: 100px
+
+  @media (max-width: 1500px)
     gap: 15px
     margin-left: 50px
     margin-right: 50px
